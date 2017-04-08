666c6ec10186a8810e9a13fe2db4114ac0d94ad0
zpovazsay <zpovazsay@gmail.com>
2017-04-08 10:03:43 +0200

    Add files via upload

--- bfi.asm
+++ bfi.asm
@@ -52,105 +52,103 @@ ML2:    ld   a, (bc)
 ;        inc  bc
 ;ML2:    ld   a, (bc)
 ;        cp   ">"               ; 7
-;        jr   z, OpIncDataPtr   ; 12 / 7
+;        jr   z, OP_IncDataPtr   ; 12 / 7
 ;        cp   "<"
-;        jr   z, OpDecDataPtr
+;        jr   z, OP_DecDataPtr
 ;        cp   "["
-;        jr   z, OpJmpForward
+;        jr   z, OP_JmpFw
 ;        cp   "]"
-;        jr   z, OpJmpBack
+;        jr   z, OP_JmpBack
 ;        cp   "+"
-;        jr   z, OpIncByte
+;        jr   z, OP_IncValue
 ;        cp   ","
-;        jr   z, OpInChar
+;        jr   z, OP_InChar
 ;        cp   "-"
-;        jr   z, OpDecByte
+;        jr   z, OP_DecValue
 ;        cp   "."
-;        jr   z, OpOutChar
+;        jr   z, OP_OutChar
 ;        or   a
 ;        ret  z
 ;        jp   MainLoop
 
 ; ===============================================
 
-retu:   ld   a, 0xf5
+OP_Return:
+        ld   a, 0xf5
         ld   (0x38), a
         ret
 
-OpIncDataPtr:
+OP_IncDataPtr:
         inc  de
         jp   (ix)
 
-OpDecDataPtr:
+OP_DecDataPtr:
         dec  de
         jp   (ix)
 
-OpIncByte:
+OP_IncValue:
         ld   a, (de)
         inc  a
         ld   (de), a
         jp   (ix)
 
-OpDecByte:
+OP_DecValue:
         ld   a, (de)
         dec  a
         ld   (de), a
         jp   (ix)
 
-OpOutChar:
+OP_OutChar:
         ld   a, (de)
         cp   10
-        jr   nz, not10
+        jr   nz, _not10
         ld   a, 13
         call PutChar
         ld   a, 10
-not10:  call PutChar
+_not10: call PutChar
         jp   (ix)
 
-OpInChar:
+OP_InChar:
         ld   a, 0xf5
         ld   (0x38), a
         call CursorOn
         call GetChar
         call PutChar
         cp   13
-        jr   nz, not13
+        jr   nz, _not13
         ld   a, 10
         call PutChar
-not13:  ld   (de), a
+_not13: ld   (de), a
         call CursorOff
         ld   a, 0xc9
         ld   (0x38), a
         jp   (ix)
 
-OpJmpForward:
+OP_JmpFw:
         ld   a, (de)
         or   a
         jp   nz, noJumpFw
         ld   l, a           ; L = 0
 
-LeftSquareBracketFound:
+_leftSquareBracketFound:
         inc  l
-jfLoop: inc  bc
+_loop1: inc  bc
         ld   a, (bc)
         cp   "["
-        jp   z, LeftSquareBracketFound
+        jp   z, _leftSquareBracketFound
         cp   "]"
-        jp   nz, jfLoop
-        ; falls to RightSquareBracketFound
-
-RightSquareBracketFound:
+        jp   nz, _loop1
         dec  l
-        jp   nz, jfLoop
+        jp   nz, _loop1
         jp   (ix)
 
-OpJmpBack:
+OP_JmpBack:
         ld   a, (de)
         or   a
-        jp   z, doNotJumpBack
+        jp   z, _doNotJumpBack
         pop  bc
         jp   ML2
-doNotJumpBack:
+_doNotJumpBack:
         pop  af                     ; drop top of the stack (address of matching '[' command)
         jp   (ix)
 
@@ -229,13 +227,13 @@ _c2:    ld   (de), a
         jr   _c1
 
 compressedOpcodeTable:
-    db   LOW retu
+    db   LOW OP_Return
     db   128 + 42, LOW MainLoop
-    db   LOW OpIncByte, LOW OpInChar, LOW OpDecByte, LOW OpOutChar
+    db   LOW OP_IncValue, LOW OP_InChar, LOW OP_DecValue, LOW OP_OutChar
     db   128 + 13, LOW MainLoop
-    db   LOW OpDecDataPtr, LOW MainLoop, LOW OpIncDataPtr
+    db   LOW OP_DecDataPtr, LOW MainLoop, LOW OP_IncDataPtr
     db   128 + 28, LOW MainLoop
-    db   LOW OpJmpForward, LOW MainLoop, LOW OpJmpBack
+    db   LOW OP_JmpFw, LOW MainLoop, LOW OP_JmpBack
     db   128 + 35, LOW MainLoop
     db   128 + 127, LOW MainLoop
     db   0
